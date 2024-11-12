f40d8e213e8728318e2379ac2b1478def9baf7b8
Дмитрий Лысенко <dmitriy7803@gmail.com>
2024-11-12 22:12:34 +0200

    edit form template

--- templates/.default/components/bitrix/form.result.new/custom_form/template.php
+++ templates/.default/components/bitrix/form.result.new/custom_form/template.php
@@ -2,103 +2,143 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
-//print_r($arResult);
-//die();
 
 /**
  * @var array $arResult
  */
-if ($arResult["isFormErrors"] == "Y"):
-    ?><?= $arResult["FORM_ERRORS_TEXT"]; ?><? endif; ?>
-<?= $arResult["FORM_NOTE"] ?? '' ?>
-<?
-if ($arResult["isFormNote"] != "Y") {
+if ($arResult["isFormErrors"] == "Y") :
     ?>
-    <?= $arResult["FORM_HEADER"] ?>
-    <table>
-        <?
-        if ($arResult["isFormDescription"] == "Y" || $arResult["isFormTitle"] == "Y" || $arResult["isFormImage"] == "Y") {
-            ?>
-            <tr>
-                <td><?
-                    if ($arResult["isFormTitle"]) {
-                        ?>
-                        <h3><?= $arResult["FORM_TITLE"] ?></h3>
-                        <?
-                    } //endif ;
+    <?= $arResult["FORM_ERRORS_TEXT"]; ?>
+<?php endif; ?>
 
-                    if ($arResult["isFormImage"] == "Y") {
-                        ?>
-                        <a href="<?= $arResult["FORM_IMAGE"]["URL"] ?>" target="_blank" alt="<?= GetMessage("FORM_ENLARGE") ?>"><img src="<?= $arResult["FORM_IMAGE"]["URL"] ?>" <? if ($arResult["FORM_IMAGE"]["WIDTH"] > 300): ?>width="300" <? elseif ($arResult["FORM_IMAGE"]["HEIGHT"] > 200): ?>height="200"<? else: ?><?= $arResult["FORM_IMAGE"]["ATTR"] ?><? endif; ?> hspace="3" vscape="3" border="0" alt=""/></a>
-                        <? //=$arResult["FORM_IMAGE"]["HTML_CODE"]?>
-                        <?
-                    } //endif
-                    ?>
+<?= $arResult["FORM_NOTE"] ?? '' ?>
 
-                    <p><?= $arResult["FORM_DESCRIPTION"] ?></p>
-                </td>
-            </tr>
-            <?
-        } // endif
-        ?>
-    </table>
-    <br />
-    <table class="form-table data-table">
+<?php if ($arResult["isFormNote"] != "Y") : ?>
 
-        <tbody>
-            <?
-            foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) {
-                if ($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == 'hidden') {
-                    echo $arQuestion["HTML_CODE"];
-                } else {
-                    ?>
-                    <tr>
-                        <td>
-                            <? if (isset($arResult["FORM_ERRORS"][$FIELD_SID])): ?>
-                                <span class="error-fld" title="<?= htmlspecialcharsbx($arResult["FORM_ERRORS"][$FIELD_SID]) ?>"></span>
-                            <? endif; ?>
-                            <?= $arQuestion["CAPTION"] ?><? if ($arQuestion["REQUIRED"] == "Y"): ?><?= $arResult["REQUIRED_SIGN"]; ?><? endif; ?>
-                            <?= $arQuestion["IS_INPUT_CAPTION_IMAGE"] == "Y" ? "<br />" . $arQuestion["IMAGE"]["HTML_CODE"] : "" ?>
-                        </td>
-                        <td><?= $arQuestion["HTML_CODE"] ?></td>
-                    </tr>
-                    <?
-                }
-            } //endwhile
-            ?>
-            <?
-            if ($arResult["isUseCaptcha"] == "Y") {
-                ?>
-                <tr>
-                    <th colspan="2"><b><?= GetMessage("FORM_CAPTCHA_TABLE_TITLE") ?></b></th>
-                </tr>
-                <tr>
-                    <td>&nbsp;</td>
-                    <td><input type="hidden" name="captcha_sid" value="<?= htmlspecialcharsbx($arResult["CAPTCHACode"]); ?>" /><img src="/bitrix/tools/captcha.php?captcha_sid=<?= htmlspecialcharsbx($arResult["CAPTCHACode"]); ?>" width="180" height="40" alt=""/></td>
-                </tr>
-                <tr>
-                    <td><?= GetMessage("FORM_CAPTCHA_FIELD_TITLE") ?><?= $arResult["REQUIRED_SIGN"]; ?></td>
-                    <td><input type="text" name="captcha_word" size="30" maxlength="50" value="" class="inputtext" /></td>
-                </tr>
-                <?
-            } // isUseCaptcha
-            ?>
-        </tbody>
-        <tfoot>
-            <tr>
-                <th colspan="2">
-                    <input <?= (intval($arResult["F_RIGHT"]) < 10 ? "disabled=\"disabled\"" : ""); ?> type="submit" name="web_form_submit" value="<?= htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == '' ? GetMessage("FORM_ADD") : $arResult["arForm"]["BUTTON"]); ?>" />
-                    <? if ($arResult["F_RIGHT"] >= 15): ?>
-                        &nbsp;<input type="hidden" name="web_form_apply" value="Y" /><input type="submit" name="web_form_apply" value="<?= GetMessage("FORM_APPLY") ?>" />
-                    <? endif; ?>
-                    &nbsp;<input type="reset" value="<?= GetMessage("FORM_RESET"); ?>" />
-                </th>
-            </tr>
-        </tfoot>
-    </table>
-    <p>
-        <?= $arResult["REQUIRED_SIGN"]; ?> - <?= GetMessage("FORM_REQUIRED_FIELDS") ?>
-    </p>
-    <?= $arResult["FORM_FOOTER"] ?>
-    <?
-} //endif (isFormNote)
+    <div class="contact-form">
+        <div class="contact-form__head">
+            <div class="contact-form__head-title">
+                <?php if ($arResult["isFormTitle"]) : ?>
+                    <h3><?= $arResult["FORM_TITLE"] ?></h3>
+                <?php endif; ?>
+            </div>
+            <div class="contact-form__head-text">
+                <?php if ($arResult["isFormDescription"]) : ?>
+                    <h3><?= $arResult["FORM_DESCRIPTION"] ?></h3>
+                <?php endif; ?>
+            </div>
+        </div>
+
+        <?php // $arResult["FORM_HEADER"]    ?>
+        <form name="<?= $arResult["WEB_FORM_NAME"] ?>" action="<?= POST_FORM_ACTION_URI ?>" method="POST"  class="contact-form__form" enctype="multipart/form-data">
+            <input type="hidden" name="WEB_FORM_ID" value="<?= $arParams["WEB_FORM_ID"] ?>">
+            <?= bitrix_sessid_post() ?>
+
+            <div class="contact-form__form-inputs">
+
+                <div class="input contact-form__input">
+                    <label class="input__label" for="medicine_name">
+                        <div class="input__label-text">
+                            <?= $arResult["QUESTIONS"]['medicine_name']['CAPTION'] ?>
+                            <?php if ($arResult["QUESTIONS"]['medicine_name']['REQUIRED'] === 'Y') : ?>
+                                <span class="form-required starrequired">*</span>
+                            <?php endif; ?>
+                        </div>
+                        <?= $arResult["QUESTIONS"]['medicine_name']['HTML_CODE'] ?>
+                        <div class="input__notification">
+                            <?= $arResult['FORM_ERRORS']['medicine_name'] ?? '' ?>
+                        </div>
+                    </label>
+                </div>
+
+                <div class="input contact-form__input">
+                    <label class="input__label" for="medicine_company">
+                        <div class="input__label-text">
+                            <?= $arResult["QUESTIONS"]['medicine_company']['CAPTION'] ?>
+                            <?php if ($arResult["QUESTIONS"]['medicine_company']['REQUIRED'] === 'Y') : ?>
+                                <span class="form-required starrequired">*</span>
+                            <?php endif; ?>
+                        </div>
+                        <?= $arResult["QUESTIONS"]['medicine_company']['HTML_CODE'] ?>
+                        <div class="input__notification">
+                            <?= $arResult['FORM_ERRORS']['medicine_company'] ?? '' ?>
+                        </div>
+                    </label>
+                </div>
+
+                <div class="input contact-form__input">
+                    <label class="input__label" for="medicine_email">
+                        <div class="input__label-text">
+                            <?= $arResult["QUESTIONS"]['medicine_email']['CAPTION'] ?>
+                            <?php if ($arResult["QUESTIONS"]['medicine_email']['REQUIRED'] === 'Y') : ?>
+                                <span class="form-required starrequired">*</span>
+                            <?php endif; ?>
+                        </div>
+                        <?= $arResult["QUESTIONS"]['medicine_email']['HTML_CODE'] ?>
+                        <div class="input__notification">
+                            <?= $arResult['FORM_ERRORS']['medicine_email'] ?? '' ?>
+                        </div>
+                    </label>
+                </div>
+
+                <div class="input contact-form__input">
+                    <label class="input__label" for="medicine_phone">
+                        <div class="input__label-text">
+                            <?= $arResult["QUESTIONS"]['medicine_phone']['CAPTION'] ?>
+                            <?php if ($arResult["QUESTIONS"]['medicine_phone']['REQUIRED'] === 'Y') : ?>
+                                <span class="form-required starrequired">*</span>
+                            <?php endif; ?>
+                        </div>
+                        <?= $arResult["QUESTIONS"]['medicine_phone']['HTML_CODE'] ?>
+                        <div class="input__notification">
+                            <?= $arResult['FORM_ERRORS']['medicine_phone'] ?? '' ?>
+                        </div>
+                    </label>
+                </div>
+            </div>
+
+            <div class="contact-form__form-message">
+                <div class="input">
+                    <label class="input__label" for="medicine_message">
+                        <div class="input__label-text">
+                            <?= $arResult["QUESTIONS"]['medicine_message']['CAPTION'] ?>
+                            <?= ($arResult["QUESTIONS"]['medicine_message']['REQUIRED'] === 'Y' ? ' *' : '') ?>
+                        </div>
+                        <?= $arResult["QUESTIONS"]['medicine_message']['HTML_CODE'] ?>
+                        <div class="input__notification">
+                            <?= $arResult['FORM_ERRORS']['medicine_message'] ?? '' ?>
+                        </div>
+                    </label>
+                </div>
+            </div>
+
+            <div class="contact-form__bottom">
+                <div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что
+                    ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных
+                    данных&raquo;.
+                </div>
+
+
+                <input type="hidden" name="web_form_submit" value="Y"/>
+
+                <button class="form-button contact-form__bottom-button" data-success="Отправлено"
+                        data-error="Ошибка отправки">
+                    <div class="form-button__title">
+                        <?= htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == '' ? GetMessage("FORM_ADD") : $arResult["arForm"]["BUTTON"]); ?>
+                    </div>
+                </button>
+            </div>
+            <?= $arResult["FORM_FOOTER"] ?>
+    </div>
+
+    <?php
+
+
+
+
+
+
+
+
+
+ endif;
